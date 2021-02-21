3ab2feab82b1755e44f6b8a1319414ac271aa104
allenwu5 <arashilen@gmail.com>
2021-02-21 16:04:42 +0800

    reduce checked name abbr.

--- helper.py
+++ helper.py
@@ -5,26 +5,49 @@ import pandas as pd
 prefixs = defaultdict(int)
 
 # https://www.kaggle.com/imoore/titanic-the-only-notebook-you-need-to-see/notebook
-personal_titles = set(t.lower() for t in ["Dr.",
-                                          "Jr.",
-                                          "Mr.",
+personal_titles = set(t.lower() for t in ["Mr.",
+                                          #   "Miss.",  != Miss
                                           "Mrs.",
-                                          "Ms.",
-                                          "Sr.",
-                                          "Miss",
-                                          "Ms",
+
                                           ])
 
 
 def extract_feature(X):
+
+    # Int64Index: 1309 entries, 0 to 417
+    # Data columns (total 12 columns):
+    # #   Column       Non-Null Count  Dtype
+    # ---  ------       --------------  -----
+    # 0   PassengerId  1309 non-null   int64
+    # 1   Survived     891 non-null    float64
+    # 2   Pclass       1309 non-null   int64
+    # 3   Name         1309 non-null   object
+    # 4   Sex          1309 non-null   object
+    # 5   Age          1046 non-null   float64
+    # 6   SibSp        1309 non-null   int64
+    # 7   Parch        1309 non-null   int64
+    # 8   Ticket       1309 non-null   object
+    # 9   Fare         1308 non-null   float64
+    # 10  Cabin        295 non-null    object
+    # 11  Embarked     1307 non-null   object
+
+    # Name ...
     X['NameWordCount'] = X['Name'].apply(lambda x: len(x.split()))
     X['Personal Title'] = X['Name'].apply(extract_personal_title)
 
-    # X['Embarked'] = X['Embarked'].fillna('C').map(
-    #     {'S': 0, 'C': 1, 'Q': 2}).astype(int)
+    # For numeric data, "Age" missing 20% records, "Fare" missing only 1 record.
+    # Age of couple might be close
+    #   Name, Age, Ticket
+    #   Clark, Mr. Walter Miller	27	13508
+    #   Clark, Mrs. Walter Miller (Virginia McDowell)	26	13508
 
     X['Age'] = X['Age'].fillna(X['Age'].median())
     X['Fare'] = X['Fare'].fillna(X['Fare'].median())
+
+    # For categorical data, "Embarked" missing 2 records, "Cabin" missing 77% records.
+    # Most of "Embarked" are "S"
+    # Do NOT turn it into integer, which performs bad ...
+    # X['Embarked'] = X['Embarked'].fillna('S')
 
     X['CabinInitChar'] = X['Cabin'].fillna(
         '').apply(extract_cabin_initial_char)
@@ -96,10 +119,10 @@ def extract_ticket_prefix(s):
     return prefix[0:1]
 
 
-def extract_personal_title(str):
+def extract_personal_title(s):
     # abbreviations: titles with personal names
     #   https://www.btb.termiumplus.gc.ca/tpv2guides/guides/wrtps/index-eng.html?lang=eng&lettr=indx_catlog_a&page=9NBnYuQ324Yc.html
-    for token in str.lower().split(" "):
+    for token in s.lower().split(" "):
         if token in personal_titles:
             return token
     return ""
